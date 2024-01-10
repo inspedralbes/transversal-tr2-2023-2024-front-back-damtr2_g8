c1e2c391d1ae8bdd01f925b97929940dc6c4e548
ivetetetete <ivettefcb@gmail.com>
2024-01-10 10:17:49 +0100

    responsive

--- vuetify/proyecto2/src/components/Game.vue
+++ vuetify/proyecto2/src/components/Game.vue
@@ -16,8 +16,9 @@ export default {
       hit: null,
       mouthPlayer1: "teethSmile",
       mouthPlayer2: "teethSmile",
-      vidaRestada1: null,
-      vidaRestada2: null,
+      //vidaRestada: null,
+      vidaRestada1: '',
+      vidaRestada2: '',
       usuaris: {
         vidaAnterior1: 100,
         vidaAnterior2: 100,
@@ -77,8 +78,10 @@ export default {
 
       if (state.partida.jugadores[this.idPlayer].vida < this.usuaris.vidaAnterior1) {
         this.hit = 0;
+        //this.vidaRestada = state.partida.jugadores[this.idPlayer].vida - this.usuaris.vidaAnterior1;
         this.vidaRestada1 = state.partida.jugadores[this.idPlayer].vida - this.usuaris.vidaAnterior1;
-        console.log(vidaRestada);
+
+        //console.log(vidaRestada);
         this.usuaris.vidaAnterior1 = state.partida.jugadores[this.idPlayer].vida;
         setTimeout(() => {
           this.hit = null;
@@ -95,8 +98,11 @@ export default {
 
       if (state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida < this.usuaris.vidaAnterior2) {
         this.hit = 1;
-        this.vidaRestada2 = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida - this.usuaris.vidaAnterior2;
-        console.log(vidaRestada);
+        //this.vidaRestada = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida - this.usuaris.vidaAnterior2;
+       // console.log(vidaRestada);
+
+       this.vidaRestada2 = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida - this.usuaris.vidaAnterior2;
+
         this.usuaris.vidaAnterior2 = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida;
         //this.mouth = "unimpressed";
         this.mouthPlayer2 = "unimpressed";
@@ -125,20 +131,20 @@ export default {
         <v-row class="px-12 py-5" style="margin: 0" v-if="!emptyGameData">
           <v-col>
             <h2>{{ setPartida.jugadores[idPlayer].username }}</h2>
-            <v-card class="PS-container" :class="{ shake: hit == 0, damageAnimation: hit == 0 }">
+            <div class="PS-container" :class="{ shake: hit == 0, damageAnimation: hit == 0 }">
               <div class="PS" v-bind:style="{ width: setPartida.jugadores[idPlayer].vida + '%', }">
                 <p>{{ setPartida.jugadores[idPlayer].vida }}</p>
               </div>
-            </v-card>
+            </div>
           </v-col>
           <v-col align="right">
             <h2 v-if="hit == 1"></h2>
             <h2>{{ setPartida.jugadores[idPlayer == 1 ? 0 : 1].username }}</h2>
-            <v-card class="PS-container" :class="{ shake: hit == 1, damageAnimation: hit == 1 }" align="left">
+            <div class="PS-container" :class="{ shake: hit == 1, damageAnimation: hit == 1 }" align="left">
               <div class="PS" v-bind:style="{ width: setPartida.jugadores[idPlayer == 1 ? 0 : 1].vida + '%',}">
                 <p>{{ setPartida.jugadores[idPlayer == 1 ? 0 : 1].vida }}</p>
               </div>
-            </v-card>
+            </div>
           </v-col>
         </v-row>
       </div>
@@ -148,7 +154,7 @@ export default {
             <v-container class="avatar-container no-bottom-lg " id="avatar-one">
               <v-img :src="'https://api.dicebear.com/7.x/big-smile/svg?seed=' +
                 setPartida.jugadores[idPlayer].avatar + '&scale=100&flip=false&eyes=angry&mouth=' + mouthPlayer1" alt="Avatar" style="width: 300px" />
-              <span>{{ this.vidaRestada1 }}</span>
+              <!-- <span class="damage-container1">{{ vidaRestada1 }}</span> -->
             </v-container>
           </v-col>
           <v-col cols="6">
@@ -164,7 +170,7 @@ export default {
           </v-col>
           <v-col cols="3">
             <v-container class="avatar-container no-bottom-lg "  id="avatar-two">
-              <span>{{ this.vidaRestada1 }}</span>
+              <!-- <span class="damage-container2">{{ vidaRestada2 }}</span> -->
               <v-img :src="'https://api.dicebear.com/7.x/big-smile/svg?seed=' +
                 setPartida.jugadores[idPlayer == 1 ? 0 : 1].avatar +
                 '&scale=100&flip=true&eyes=angry&mouth=' + mouthPlayer2
@@ -196,8 +202,16 @@ export default {
 </template>
 
 <style scoped>
+  .avatar-container#avatar-one{
+    margin-right: 15px;
+  }
+
+  .avatar-container#avatar-two{
+    margin-left: 20px;
+  }
 .my-app-background{
   background-color: lightblue;
+  overflow: hidden;
 }
 .game-container {
   height: 100vh;
@@ -206,6 +220,21 @@ export default {
   display: flex;
 }
 
+.damage-container1 {
+  position: absolute;
+  background-color: blueviolet;
+  margin-top: 30px;
+  margin-left: 250px;
+  font-size: 45px;
+}
+
+.damage-container2 {
+  position: absolute;
+  background-color: blueviolet;
+  margin-top: 30px;
+  margin-left: 20px;
+  font-size: 45px;
+}
 .input-operation {
   width: 500px;
   margin-top: 20px;
@@ -319,9 +348,10 @@ export default {
   }
 
   .operation-box {
-    width: 500px;
+    width: 450px;
     height: 80px;
     margin-top: 40px;
+    background-color: blueviolet;
   }
 
   .input-operation {
