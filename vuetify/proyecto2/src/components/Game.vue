36bd3b038b35a0263634f4242d1dcce22d4d1223
oscar <a22oscmungar@inspedralbes.cat>
2024-01-10 13:21:07 +0100

    arreglar

--- vuetify/proyecto2/src/components/Game.vue
+++ vuetify/proyecto2/src/components/Game.vue
@@ -91,22 +91,18 @@ export default {
           this.hit = null;
         }, 100);
 
-        this.mouthPlayer1 = "unimpressed";
+        //this.mouthPlayer1 = "unimpressed";
 
-        if(state.partida.jugadores[this.idPlayer].vida == 100){
+        if(this.vidaRestada1==0){
           this.mouthPlayer1="teethSmile&skinColor=efcc9f";
         }else if(this.usuaris.vidaAnterior1 <= 10){
           this.mouthPlayer1="unimpressed&skinColor=F44336";
         }else if (this.usuaris.vidaAnterior1 == 0){
-          this.mouthPlayer2 ="openedSmile";
+          this.mouthPlayer2 ="openedSmile&skinColor=efcc9f";
           party.confetti();
           party.sparkles();
         }else{
           this.mouthPlayer1="unimpressed";
-        if (this.usuaris.vidaAnterior1 <= 10) {
-          this.mouthPlayer1 = "unimpressed&skinColor=F44336";
-        } else if (this.usuaris.vidaAnterior1 == 0) {
-          this.mouthPlayer2 = "openedSmile";
         }
       }
 
@@ -115,18 +111,18 @@ export default {
         //this.vidaRestada = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida - this.usuaris.vidaAnterior2;
        // console.log(vidaRestada);
 
-       this.vidaRestada2 = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida - this.usuaris.vidaAnterior2;
+        this.vidaRestada2 = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida - this.usuaris.vidaAnterior2;
 
         this.usuaris.vidaAnterior2 = state.partida.jugadores[this.idPlayer == 1 ? 0 : 1].vida;
         //this.mouth = "unimpressed";
         
 
-        if(state.partida.jugadores[this.idPlayer].vida == 100){
+        if(this.vidaRestada2==0){
           this.mouthPlayer2="teethSmile&skinColor=efcc9f";
         }else if(this.usuaris.vidaAnterior2 <= 10){
           this.mouthPlayer2="unimpressed&skinColor=F44336";
         }else if (this.usuaris.vidaAnterior2 == 0){
-          this.mouthPlayer1 ="openedSmile";
+          this.mouthPlayer1 ="openedSmile&skinColor=efcc9f";
           party.confetti();
           party.sparkles();
         }else{
@@ -139,7 +135,7 @@ export default {
       }
 
       return state.partida;
-    },
+      }
   },
 };
 </script>
@@ -218,7 +214,7 @@ export default {
         </v-row>
       </v-container>
     </div>
-  </div>
+  </v-app>
 </template>
 
 <style scoped>
@@ -240,19 +236,21 @@ export default {
   display: flex;
 }
 
-@media only screen and (min-width: 960px) and (max-width: 1919px) {
-  .avatar-container {
-    position: relative;
-    bottom: 50px;
-  }
+.damage-container1 {
+  position: absolute;
+  background-color: blueviolet;
+  margin-top: 30px;
+  margin-left: 250px;
+  font-size: 45px;
 }
 
-@media only screen and (min-width: 1920px) {
-  .no-bottom-lg {
-    margin-bottom: 0 !important;
-  }
+.damage-container2 {
+  position: absolute;
+  background-color: blueviolet;
+  margin-top: 30px;
+  margin-left: 20px;
+  font-size: 45px;
 }
-
 .input-operation {
   width: 500px;
   margin-top: 20px;
@@ -260,7 +258,6 @@ export default {
 }
 
 .bottom-aligned-col {
-  position: fixed;
   bottom: 20px;
   width: 100%;
   text-align: center;
@@ -301,11 +298,6 @@ export default {
   font-size: 23px;
   width: 60%;
   background-color: rgb(153, 153, 153);
-}
-
-.game-container {
-  background: lightblue;
-  height: 100vh;
 }
 
 .content-wrap {
@@ -353,6 +345,124 @@ export default {
   animation: shake 0.12s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
   transform: translate3d(0, 0, 0);
 }
+@media only screen and (max-width: 830px){
+  .avatar-container{
+    margin: 0;
+  }
+  .avatar-container#avatar-one{
+    margin-right: 15px;
+  }
+
+  .avatar-container#avatar-two{
+    margin-left: 15px;
+  }
+
+  .operation-label {
+    position: relative;
+    bottom: 10px;
+    font-size: 47px;
+  }
+
+  .operation-box {
+    width: 450px;
+    height: 80px;
+    margin-top: 40px;
+    background-color: blueviolet;
+  }
+
+  .input-operation {
+    margin-top: 5px;
+  }
+
+  .dificulty-option {
+    margin-top: 20px;
+    font-size: 25px !important;
+    font-weight: 800 !important;
+    height: 100px !important;
+    width: 200px;
+  }
+}
+@media only screen and (min-width:831px) and (max-width:960px){
+  .avatar-container{
+    margin: 0;
+  }
+  .avatar-container#avatar-one{
+    margin-right: 15px;
+  }
+
+  .avatar-container#avatar-two{
+    margin-left: 15px;
+  }
+
+  .operation-label {
+    position: relative;
+    bottom: 10px;
+    font-size: 47px;
+  }
+
+  .operation-box {
+    width: 500px;
+    height: 80px;
+    margin-top: 10px;
+  }
+
+  .input-operation {
+    margin-top: 5px;
+  }
+
+  .btnSolve{
+    margin-bottom: 15px;
+  }
+
+  
+
+}
+
+@media only screen and (min-width: 960px) and (max-width: 1280px) {
+  .game-container {
+    height: 100vh;
+  }
+
+  .operation-label {
+    position: relative;
+    bottom: 10px;
+    font-size: 47px;
+  }
+
+  .operation-box {
+    width: 500px;
+    height: 80px;
+    margin-top: 10px;
+  }
+
+  .input-operation {
+    margin-top: 5px;
+  }
+
+  .btnSolve{
+    margin-bottom: 15px;
+  }
+
+}
+
+@media only screen and (max-width: 1919px) {
+  .avatar-container {
+    position: relative;
+    bottom: 50px;
+  }
+}
+
+@media only screen and (min-width: 1920px) {
+  .no-bottom-lg {
+    margin-bottom: 0 !important;
+  }
+
+  .btnSolve{
+    margin-top: -3px;
+  }
+}
+
+
 
 @keyframes shake {
   0% {
