26bbb5883df037c1dc29e4afc98a20f45e9d1329
Marti-SV <a21marsalval@inspedralbes.cat>
2024-01-10 12:26:26 +0100

    Fix error handling in server.js

--- backend/server.js
+++ backend/server.js
@@ -265,8 +265,7 @@ function ejecutarEstadisticas(idClasse) {
     });
 
     await getClassNameByClassId(idClasse).then((data) => {
-      console.log(data);
-      //data.err == null ? nomClasse = data[0].nomClasse : null
+      data.err != undefined ? nomClasse = data[0].nomClasse : null
     });
     let pythonProcess = spawn("python3", ["./stats.py", JSON.stringify(arrayUsuarios), nomClasse, idClasse]);
     pythonProcess.stdout.on("data", (data) => resolve(data.toString()));
